04ac76f54aa8f5ceb098475396a802c724af9817
luisguarnizo1234 <gonzalo.guarnizo.garcia@gmail.com>
2022-09-14 16:13:04 -0500

    se cambió la conversión de fechas, eliminando el *1000

--- src/main/java/com/fac/facgg/controllers/SatelliteController.java
+++ src/main/java/com/fac/facgg/controllers/SatelliteController.java
@@ -56,7 +56,7 @@ public class SatelliteController {
         List<Double> dataDtos = new ArrayList<>();
 
         for (Satellite satellite : satellites) {
-            Date date = new Date(satellite.getTs() * 1000);
+            Date date = new Date(satellite.getTs() );
             SimpleDateFormat DateFor = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
             String stringDate = DateFor.format(date);
             dateDtos.add(stringDate);
@@ -130,7 +130,7 @@ public class SatelliteController {
         List<Double> dataDtos = new ArrayList<>();
 
         for (Satellite satellite : satellites) {
-            Date date = new Date(satellite.getTs() * 1000);
+            Date date = new Date(satellite.getTs());
             SimpleDateFormat DateFor = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
             String stringDate = DateFor.format(date);
             dateDtos.add(stringDate);
